8e11e24162582ecfa624d509cb29a9f2e42e28ee
brendaoby <brendaekemezie@gmail.com>
2022-06-30 10:20:54 -0700

    Printing a statement to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Point of entry
+ * imain - Prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19 to the standard error
  *
  * Return: Always 1 (Error)
  */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
-	return (1)
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
