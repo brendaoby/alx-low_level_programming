e4df5555723dae37ac2eba116786af89fd23cbe2
brendaoby <brendaekemezie@gmail.com>
2022-07-01 12:35:35 -0700

    Displays all possible combos of 0 to 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,13 +12,13 @@ int i;
 
 for (i = 0; i <= 9; ++i)
 {
-	putchar('0' + i);
-	
-	if (i < 9)
-	{
-		putchar(',');
-		putchar(' ');
-	}
+putchar('0' + i);
+
+if (i < 9)
+{
+putchar(',');
+putchar(' ');
+}
 }
 putchar('\n');
 return (0);
