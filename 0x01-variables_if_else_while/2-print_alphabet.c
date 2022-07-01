1f1466d3ea34826aa78535d4390880a590c974e1
brendaoby <brendaekemezie@gmail.com>
2022-07-01 08:39:54 -0700

    Displayong lowercase alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,14 +2,14 @@
 
 /**
  * main - Entry point
- * Return - Always 0(Success)
+ * Return - Always 0 (Success)
  */
 int main(void)
 {
 	char b;
 
 	for (b = 'a'; b <= 'z'; b++)
-		putchar(b);
-		putchar('\n');
+	putchar(b);
+	putchar('\n');
 	return (0);
 }
