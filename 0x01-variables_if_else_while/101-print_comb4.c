849ecb11eade5da63b33f454b63f3597487c4a9a
brendaoby <brendaekemezie@gmail.com>
2022-07-01 14:49:24 -0700

    Displays combos of three numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,31 +8,30 @@
  */
 int main(void)
 {
-int a;
-int b;
-int c;
+	int a;
+	int b;
+	int c;
 
-for (a = 48; a < 58; a++)
-{
-for (b = 49; b < 58; b++)
-{
-for (c = 50; c < 58; c++)
-{
-if (c > b && b > a)
-{
-putchar(a);
-putchar(b);
-putchar(c);
-if (a != 55 || b != 56)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-}
-}
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 49; b < 58; b++)
+		{
+			for (c = 50; c < 58; c++)
+			{
+				if (c > b && b > a)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55 || b != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
 putchar('\n');
 return (0)
 }
