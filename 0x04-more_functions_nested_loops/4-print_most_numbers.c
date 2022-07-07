c5d0a3f357a0c63d321a15b3b4af59937c40c761
brendaoby <brendaekemezie@gmail.com>
2022-07-07 07:44:24 -0700

    Digit Printer .2

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints all digits, but 2 and 4
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int b;
+
+	for (b = 48; b < 58; b++)
+	{
+		if (b != 50 && b != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -2,3 +2,4 @@
 1. Checks for a digit (0 through 9).
 2. Multiplies two integers.
 3. Prints the numbers, from 0 to 9, followed by a new line.
+5. Prints the numbers, from 0 to 9, except 2 and 4, followed by a new line.
