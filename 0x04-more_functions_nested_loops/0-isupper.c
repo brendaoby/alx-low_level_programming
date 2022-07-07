21e84893d533b2c2b62f4a98ea290745353a0092
brendaoby <brendaekemezie@gmail.com>
2022-07-07 07:05:08 -0700

    Uppercase Checker

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper: Checks for Uppercase Letter
+ * _isupper - Checks for Uppercase Letter
  * @c: Value to be checked
  * Return: 1 if uppercase, 0 if otherwise
  */
