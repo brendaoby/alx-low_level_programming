cfa36cb0e5338615cc928cfae30ce255f50db2da
brendaoby <brendaekemezie@gmail.com>
2022-07-07 08:46:28 -0700

    Square Printer

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square
+ * @size: Parameter
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int b = 0;
+
+	int q;
+
+	if (size > 0)
+	{
+		for (; b < size; b++)
+		{
+			for (q = 0; q < size; q++)
+			{
+				_putchar(35);
+				_putchar('\n');
+			}
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -6,3 +6,4 @@
 5. Prints 10 times the numbers, from 0 to 14, followed by a new line.
 6. Draws a straight line in the terminal.
 7. Draws a diagonal line on the terminal.
+8. Prints a square, followed by a new line.
