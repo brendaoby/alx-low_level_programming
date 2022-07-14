f84a75980684291bf2d27b901b673b078e6fa5a9
brendaoby <brendaekemezie@gmail.com>
2022-07-14 06:51:41 -0700

    Reversal

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,31 @@
+#inlude "main.h"
+
+/**
+ * reverse_array - Reverses an array of integers
+ * @a: Pointer
+ * @n: Parameter
+ * Return: 0
+ */
+
+void reverse_array(int *a, int n);
+{
+	int startb, endb, b;
+	int x;
+
+	startb = a;
+	endb = a;
+
+	for (x = 0; x < n - 1; x++)
+	{
+		endb++;
+	}
+
+	for (x = 0; x < n / 2; x++)
+	{
+		b = *endb;
+		*endb = *startb;
+		*startb = b;
+		startb++;
+		endb--;
+	}
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@
 1. Concatenates two strings to the nth character.
 2. Copies a string up to the nth character.
 3. Compares two strings.
+4. Reverses the content of an array of integers.
