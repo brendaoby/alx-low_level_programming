5bab8a57e66be39388ab8feb4d43e5908cc78410
brendaoby <brendaekemezie@gmail.com>
2022-08-03 08:05:15 -0700

    Func Ptr

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,23 +7,26 @@
  * @size: The size of the array of integers
  * @cmp: A pointer to the function to be used
  * to compare values
- * Return: index of the first element for which 
- * the cmp function does not return or -1 if 
+ * Return: Index of the first element for which
+ * the cmp function does not return or -1 if
  * no element matches or size <= 0
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int i;
+	int index;
 
-if (array && cmp)
-{
-  for (i = 0; i < size; i++)
-  {
-    if (cmp(array[index]) != 0)
-      return (index);
-  }
-}
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
 
-return (-1);
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index]) != 0)
+		{
+			return (index);
+		}
+	}
+	return (-1);
 }
