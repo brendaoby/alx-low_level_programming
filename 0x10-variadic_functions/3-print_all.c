19567ce542758e2bf3d70fcee6b7ee67765e1fd0
Brenda Ekemezie <106753615+brendaoby@users.noreply.github.com>
2022-08-04 13:58:12 +0100

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,52 +8,44 @@
 
 void print_all(const char * const format, ...)
 {
-	va_list list;
+va_list valist;
+unsigned int i = 0, j, k = 0;
+char *str;
+const char t_arg[] = "cifs";
 
-	unsigned int i = 0, j, k = 0;
-
-	char *strg;
-
-	const char t_arg[] = "cifs";
-
-	va_start(list, format);
-
-	while (format && format[i])
-	{
-		j = 0;
-		while (t_arg[j])
-		{
-			if (format[i] == t_arg[j] && k)
-			{
-				printf(", ");
-				break;
-			}
-			j++;
-		}
-		switch (format[i])
-		{
-			case 'c':
-				printf("%c", va_arg(list, int)), k = 1;
-				break;
-			case 'i':
-				printf("%d", va_arg(list, int)), k = 1;
-				break;
-			case 'f':
-				printf("%f", va_arg(list, double)), k = 1;
-				break;
-			case 's':
-				strg = va_arg(list, char *), k = 1;
-
-				if (!strg)
-				{
-					printf("(nil)");
-					break;
-				}
-				printf("%s", strg);
-				break;
-		}
-		i++;
-	}
-	printf("\n");
-       va_end(list);
+va_start(valist, format);
+while (format && format[i])
+{
+j = 0;
+while (t_arg[j])
+{
+if (format[i] == t_arg[j] && k)
+{
+printf(", ");
+break;
+} j++;
+}
+switch (format[i])
+{
+case 'c':
+printf("%c", va_arg(valist, int)), k = 1;
+break;
+case 'i':
+printf("%d", va_arg(valist, int)), k = 1;
+break;
+case 'f':
+printf("%f", va_arg(valist, double)), k = 1;
+break;
+case 's':
+str = va_arg(valist, char *), k = 1;
+if (!str)
+{
+printf("(nil)");
+break;
+}
+printf("%s", str);
+break;
+} i++;
+}
+printf("\n"), va_end(valist);
 }
